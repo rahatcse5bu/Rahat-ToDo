becd174ffba5aa457354a34eccc3e3a1997ae69a
Md. Rahat <rahat.cse5.bu@gmail.com>
2023-11-18 17:26:26 +0600

    Localstorage based data save

--- src/components/ToDoWrapper.jsx
+++ src/components/ToDoWrapper.jsx
@@ -1,40 +1,55 @@
-/* eslint-disable react/jsx-no-undef */
-// eslint-disable-next-line no-undef
-// import React from 'react'
-import { useState } from 'react';
-import ToDoForm from './ToDoForm'
-import ToDo from './Todo'
+// eslint-disable-next-line react/jsx-no-undef
+import React, { useState, useEffect } from 'react';
+import ToDoForm from './ToDoForm';
+import ToDo from './Todo';
 import { v4 as uuidv4 } from 'uuid';
 import EditTodoForm from './EditToDoForm';
-uuidv4(); 
-// import React from 'react'
 
 function ToDoWrapper() {
-  const [todos,setTodos]= useState([]);
-  const addTodo = todo =>{
-setTodos([...todos,{id:uuidv4(),task:todo,completed:false,isEditing:false}])
-console.log(todos)
-  }
-  const editTodo = id =>{
-setTodos(todos.map(todo => todo.id===id?{...todo,isEditing: !todo.isEditing}:todo))
+  const initialTodos = JSON.parse(localStorage.getItem('todos')) || [];
+  const [todos, setTodos] = useState(initialTodos);
 
-  }
-  const updateTodo = (todoContent,id) =>{
-setTodos(todos.map(todo => todo.id===id?{...todo,task:todoContent,isEditing: !todo.isEditing}:todo))
+  const addTodo = (todo) => {
+    setTodos([...todos, { id: uuidv4(), task: todo, completed: false, isEditing: false }]);
+  };
 
-  }
-  const deleteTodo = id =>{
-    setTodos((todos) => todos.filter((todo) => todo.id !== id));
-  }
+  const editTodo = (id) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo)));
+  };
+
+  const updateTodo = (todoContent, id) => {
+    setTodos(todos.map((todo) => (todo.id === id ? { ...todo, task: todoContent, isEditing: !todo.isEditing } : todo)));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  useEffect(() => {
+    // Save todos to localStorage whenever the todos state changes
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
   return (
-  <div className="container mx-auto bg-slate--400 w-[400px] bg-[#1C1A42] rounded-md mt-4 py-4 px-4">
-  <p className='text-center text-3xl text-white py-4'>Rahat ToDo:</p>
-     {todos.length<11? (<ToDoForm addTodo={addTodo}/>):<p className='w-[400px] text-center text-3xl py-4 bg-red-400 text-white my-4'> you can not add more than 10</p>}
-    
-{ todos.map((todo,index) =>
-     todo.isEditing? (<EditTodoForm key={index} updateTodo={updateTodo} todo={todo} id={todo.id}/>): ( <ToDo key={index} task={todo} deleteTodo={deleteTodo} editTodo={editTodo}/>))}
+    <div className="container mx-auto bg-slate--400 w-[400px] bg-[#1C1A42] rounded-md mt-4 py-4 px-4">
+      <p className="text-center text-3xl text-white py-4">Rahat ToDo:</p>
+      {todos.length < 11 ? (
+        <ToDoForm addTodo={addTodo} />
+      ) : (
+        <p className="w-[400px] text-center text-3xl py-4 bg-red-400 text-white my-4">
+          You cannot add more than 10! Delete some to add more
+        </p>
+      )}
+
+      {todos.map((todo, index) =>
+        todo.isEditing ? (
+          <EditTodoForm key={index} updateTodo={updateTodo} todo={todo} id={todo.id} />
+        ) : (
+          <ToDo key={index} task={todo} deleteTodo={deleteTodo} editTodo={editTodo} />
+        )
+      )}
     </div>
-  
-)}
+  );
+}
 
 export default ToDoWrapper;
